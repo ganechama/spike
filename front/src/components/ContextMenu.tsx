01fdf23a062f8a0e93df4dfe9ef74e78dce886ab
ガーネット <magic.kitchen.factory.dev@gmail.com>
2024-10-18 10:51:21 +0900

    コメント追加

--- front/src/components/ContextMenu.tsx
+++ front/src/components/ContextMenu.tsx
@@ -3,29 +3,39 @@
 import React from 'react';
 import { useRouter } from 'next/navigation'
 
+// コンテキストメニューのProps
 interface ContextMenuProps {
+  // コンテキストメニューの座標
   contextMenu: { mouseX: number; mouseY: number } | null;
+  // コンテキストメニューを閉じる関数
   handleClose: () => void;
+  // コンテキストメニューの項目
   options: { label: string; path: string; onClick?: () => void }[];
 }
 
+// コンテキストメニュー
 const ContextMenu: React.FC<ContextMenuProps> = ({ contextMenu, handleClose, options }) => {
   const router = useRouter();
 
+  // コンテキストメニューがnullの場合は何も描画しない
   if (contextMenu === null) {
     return null;
   }
 
+  // メニューの項目をクリックした時のハンドラ
   const handleMenuClick = (option: { label: string; path: string; onClick?: () => void }) => {
+    // itemにonClickが設定されている場合はそちらを優先する
     if (option.onClick) {
       option.onClick();
     } else {
+      // コンテキストメニューを閉じて、pathに遷移する
       handleClose();
       router.push(option.path);
     }
   };
 
   return (
+    // コンテキストメニューを描画
     <ul
       className="absolute bg-white border border-gray-300 shadow-md"
       style={{ top: contextMenu.mouseY, left: contextMenu.mouseX }}
